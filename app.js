be1636a06a7fd05d03782a4fe74baa06b3e3b72f
qiu363 <qiu13141@gmail.com>
2016-05-06 17:12:40 +0800

    add jieba

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -10,11 +12,50 @@ app.get('/', function (req, res) {
 });
 
 app.get('/express', function (req, res) {
-  res.sendfile(__dirname + '/index.html'); 
+  res.sendfile(__dirname + '/www/index.html'); 
 });
 
 app.get('/express/node', function (req, res) {
   res.send('express/node');
+});
+
+app.get('/express/jieba', function (req, res) {
+  var nodejieba = require('nodejieba');
+  var s = {};
+  var w = req.query.w;
+
+  if (w) {
+    s = nodejieba.tag(w);
+    res.json(s);
+  } else {
+    res.sendfile(__dirname + '/www/jieba.html'); 
+  }
+});
+
+app.get('/express/admin', function (req, res) {
+  res.sendfile(__dirname + '/www/admin/index.html');  
+});
+
+app.get('/express/mongo', function (req, res) {
+  let mongoose = require('mongoose');
+  let db = mongoose.createConnection('mongodb://127.0.0.1:10000/haogj', 'haogj');
+  db.on('error', function (error) {
+    console.log(error);
+  });
+  
+  let mongooseSchema = new mongoose.Schema({
+    code: {type: String},
+    country: {type: String},
+    enCountry: {type: String}
+  });
+  let mongooseModel = db.model('haogj', mongooseSchema);
+  let mongooseEntity = new mongooseModel({});
+
+  mongooseModel.find(function (error, data) {
+    console.log(data); 
+  });
+
+  res.send('mongodb');
 });
 
 app.get('/express/change', function (req, res) {
@@ -35,4 +76,9 @@ io.on('connection', function (socket) {
 });
 
 app.use('/express/static', express.static('www'));
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
